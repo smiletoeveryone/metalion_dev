ae60ad1730b180512e97a2b8978edf75df8cd8dc
Scott. J <41283107+smiletoeveryone@users.noreply.github.com>
2023-02-20 16:54:19 +0800

    Update ConnectWallet.tsx

--- components/ConnectWallet.tsx
+++ components/ConnectWallet.tsx
@@ -16,25 +16,32 @@ import { useAuthContext } from "../context/AuthContext";
 import { FaDiscord, FaPhoneAlt } from "react-icons/fa";
 
 const ZH = {
-  heroTitle: "Metalion\n俱樂部 🎉",
+  heroTitle: "宙獅大旅社 🎉",
   subtitle:
     "雄獅集團的 Web3 社群平台，以旅遊為核心。\n賦能食宿遊購行領域，打造華語旅遊 Web3 最佳社群",
-  connectWalletDesc: "gonna 兌換咖啡，\n活動已於8/5結束! \n\n更多的優惠活動將持續推出...",
+  // connectWalletDesc: "gonna 兌換咖啡，\n活動已於8/5結束! \n\n更多的優惠活動將持續推出...",
+  connectWalletDesc: "😋️😋️😋️AsiaYo 特色旅宿\n\n 海內外特色旅宿/主題民宿 \n無上限最高6.5%現金回饋 (註.1)\n#包棟 #秘境 #好友出遊\n\nAsia Yo的訂房優惠即將展開 ，\n立即連結您的錢包，\n會出現專屬的折扣連結喔👍️👍️👍️",
   discordDesc: "追蹤最新活動消息，\n請加入 Metalion Discord ",
-  travelshowDesc: "Metalion Lodging Pass \n（宙獅大旅社）", //"宙獅計劃Metalion，\n搶先註冊！",
+  // travelshowDesc: "Metalion Lodging Pass \n（宙獅大旅社）", //"宙獅計劃Metalion，\n搶先註冊！",
+  travelshowDesc: "😋️😋️😋️宙獅精選飯店\n\n海內外精選星級飯店\n無上限最高6.5%現金回饋 (註.1)\n即時房源，即訂即住.👍️👍️👍️", //"宙獅計劃Metalion，\n搶先註冊！",
   redeemTitle: "連接錢包查看活動 🎉\n加入 Discord 抽獎慶祝 🎁",
   hero2Title: "宙獅元宇宙開台慶\n6/6 活動正式開跑",
   cta2Ttile: "加入Discord 一同狂歡 🎉",
-  checkRedeemBtn: "甜點兌換查詢",
+  // checkRedeemBtn: "甜點兌換查詢",
+  // connectWalletTitle_asiayo: "😍️😍️😍️ click here for visiting AsiaYo! 💝️💝️💝️",
   pleaseSignTitle: "錢包連結成功，請於錢包進行簽署完成登入",
   loginSuccessfulTitle: "登入成功",
   // btns text
   connectWalletTitle: "連接錢包",
   joinDiscordTitle: "加入 Discord",
-  travelshowVisitorTitle: "領取 NFT", // "Metalion X Qubic",
+  travelshowVisitorTitle: "登入宙獅大旅社", // "Metalion X Qubic",
   signWalletTitle: "簽署綁定",
-  enterPortalTitle: "前往兌換中心",
+  //enterPortalTitle: "前往兌換中心",
+  enterPortalTitle_asiayo: "😍️😍️😍️ 按這裏訪問Asia Yo取得您的專屬折扣❗️ 💝️💝️💝️",
+  checkRedeemBtn_qubic: "登入/註冊 Qubic 錢包❗️❗️❗️", 
+  noteDesc:"註:\n\n1. 宙獅大旅社NFT訂房使用期限為2023/12/31止.住房需於2024/12/31前入住完畢.\n2. 需透過宙獅大旅社網頁專屬連結至訂房網站. (重要)\n3. 入住完成後30天內於宙獅Discord提供收款帳號即可獲得現金回饋",
 };
+
 
 export default function ConnectWallet() {
   const { connect, isAuth } = useAuthContext();
