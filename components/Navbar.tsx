a8b855c4d5c77ae594b9725226aa977c5fdb11f8
Scott. J <41283107+smiletoeveryone@users.noreply.github.com>
2023-02-21 15:35:22 +0800

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -115,9 +115,13 @@ export default function Navbar() {
           direction={"row"}
           spacing={3}
         >
-          <NextLink passHref href="https://liontravel.qubic.market/en">
+          <NextLink passHref href="https://devtest.metalion.world/">          
             <a>
               <Button
+                as={"a"}
+                target="_blank"
+                href="https://devtest.metalion.world/"
+                rel="noreferrer"
                 variant="text"
                 fontSize={"lg"}
                 display={{ base: "none", md: "flex" }}
