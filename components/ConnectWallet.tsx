b8f0e981c236febe0e2548bea29fa20c5f35cda3
Scott. J <41283107+smiletoeveryone@users.noreply.github.com>
2023-02-24 21:32:50 +0800

    Update ConnectWallet.tsx

--- components/ConnectWallet.tsx
+++ components/ConnectWallet.tsx
@@ -355,7 +355,7 @@ export default function ConnectWallet() {
           </Heading>
           
           <Button
-            mt={-16}
+            mt={-20}
             w={{ base: "80" }}
             leftIcon={<FaDiscord />}
             // w="full"
